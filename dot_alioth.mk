ebaac0b29f7e745c098641594937a87430ea2daf
Roxor-007 <68855313+Roxor-007@users.noreply.github.com>
2022-05-28 02:44:43 +0530

    Dot bringup
    -drop fp from makefile.

--- dot_alioth.mk
+++ dot_alioth.mk
@@ -9,12 +9,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common lineage stuff.
-$(call inherit-product, vendor/corvus/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := corvus_alioth
+PRODUCT_NAME := dot_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
@@ -22,10 +22,4 @@ PRODUCT_MODEL := POCO F3
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="alioth_global-user 11 RKQ1.200826.002 V12.5.6.0.RKHMIXM release-keys"
-
-BUILD_FINGERPRINT := Redmi/alioth_global/alioth:11/RKQ1.200826.002/V12.5.6.0.RKHMIXM:user/release-keys
-
-CORVUS_MAINTAINER= Trishiraj, Roxor-007
-RAVEN_LAIR=Official
+TARGET_BOOT_ANIMATION_RES=1440
